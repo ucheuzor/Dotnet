0056a7c3c9f7eaa7bfcc53cd579e735764994e92
Urchy <ucheuzor24@gmail.com>
2024-10-18 21:24:44 +0100

    C# challenge refreshers

---
+++ C# Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string logList = "[INFO]: Operation completed";
+string specialCharacters = "\r\n";
+
+int closingParenthesisCheck = logList.IndexOf("]");
+int colonCheck = logList.IndexOf(":");
+
+if (closingParenthesisCheck != -1 && colonCheck != -1)
+{
+    string matchingItems = logList.Substring(colonCheck + 1).Trim();
+    if (matchingItems.Contains(specialCharacters))
+    {
+        matchingItems = matchingItems.Remove(matchingItems.IndexOf(specialCharacters), specialCharacters.Length);
+    }
+    Console.WriteLine(matchingItems);
+
+}
