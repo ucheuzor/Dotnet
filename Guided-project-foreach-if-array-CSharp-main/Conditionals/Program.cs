c1a51c58a0f332f6b9a5234135900a3825c97005
Urchy <ucheuzor24@gmail.com>
2024-10-21 17:25:37 +0100

    added logic to check user role based permission.

--- Guided-project-foreach-if-array-CSharp-main/Conditionals/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/Conditionals/Program.cs
@@ -1,10 +1,25 @@
-﻿
+﻿//Toss a Coin for head or til. Outcome  for each should be 50%
 Random random = new Random();
 int coinToss = random.Next(0, 2);
 
 string outcome = coinToss > 0 ? "Head" : "Tail";
 
 Console.WriteLine($"You tossed {outcome}");
+
+//- - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+string permission = "Admin|Manager";
+int level = 10;
+
+if (permission.Contains("Admin") && level > 55)
+    Console.WriteLine("Welcome, Super Admin user.");
+else if (permission.Contains("Admin") && level <= 55)
+    Console.WriteLine("Welcome, Admin user.");
+else if (permission.Contains("Manager") && level >= 20)
+    Console.WriteLine("Contact an Admin for access.");
+else if (permission.Contains("Manager") && level < 20 || !permission.Contains("Admin") || !permission.Contains("Manager"))
+    Console.WriteLine("You do not have sufficient privileges.");
+
 
 /*
 int salesAmount = 1001;
