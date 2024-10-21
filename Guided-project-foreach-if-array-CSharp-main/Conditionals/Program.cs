d806ca3748e4dafdd389e2b646c5c3e0e459c830
Urchy <ucheuzor24@gmail.com>
2024-10-21 17:07:11 +0100

    Added section for conditionals

---
+++ Guided-project-foreach-if-array-CSharp-main/Conditionals/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Random random = new Random();
+int coinToss = random.Next(0, 2);
+
+string outcome = coinToss > 0 ? "Head" : "Tail";
+
+Console.WriteLine($"You tossed {outcome}");
+
+/*
+int salesAmount = 1001;
+
+int discount = salesAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: ${discount}");
+*/
