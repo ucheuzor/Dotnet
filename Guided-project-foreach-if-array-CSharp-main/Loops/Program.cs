efdd74e053c8b7681e773692e1278c145f802f06
Urchy <ucheuzor24@gmail.com>
2024-10-21 21:54:29 +0100

    For loop sample examples

---
+++ Guided-project-foreach-if-array-CSharp-main/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//for look to loop through an array using for loop.
+string[] names = ["Uchenna", "david", "Eddie", "Samson"];
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+
+}
+Console.WriteLine("---------------------------");
+
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    if (names[i] == "Uchenna")
+        names[i] = "Uchenna Uzor";
+
+
+    Console.WriteLine(names[i]);
+}
+
+/*
+for (int i = 1; i < 20; i += 3)
+{
+
+    if (i > 15)
+        break;
+
+    Console.WriteLine(i);
+}
+*/
