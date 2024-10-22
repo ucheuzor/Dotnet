427ce78374c654afae63be14a2e128a940facb6c
Urchy <ucheuzor24@gmail.com>
2024-10-22 17:52:13 +0100

    Added a C# code to loop through an array string and print the array items in the console by splitting items where there is a Period

--- Guided-project-foreach-if-array-CSharp-main/Difference between Do While and While Loop/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/Difference between Do While and While Loop/Program.cs
@@ -1,5 +1,30 @@
 ﻿using System.Globalization;
 
+//Add a C# code to split an array of string by a period. based on the splitting, print the individual items to the console.
+
+string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+
+for (int i = 0; i < myStrings.Length; i++)
+{
+    if (myStrings[i].IndexOf('.') != -1)
+    {
+        string[] matchingString = myStrings[i].Split('.');
+
+        foreach (string stringItem in matchingString)
+        {
+            Console.WriteLine(stringItem.Trim());
+        }
+    }
+    else
+    {
+        Console.WriteLine(myStrings[i]);
+    }
+}
+
+
+
+//This simple application is used to check the user input. Based on what is inputted, we determine if the user is an admin or not
+
 string? userRole = String.Empty;
 Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
 
@@ -31,7 +56,6 @@ do
 } while (userRole?.Trim().ToLower() != "administrator" || userRole?.Trim().ToLower() != "manager" || userRole?.Trim().ToLower() != "user");
 
 
-/* 
 //This C# code using a do while loop to check a user input. Only values between 5 - 10 are allowed. If user input is outside range, prompt the user to enter input
 
 int userInput = 0;
@@ -83,4 +107,3 @@ do
     }
 
 } while (validEntry == false);
- */
