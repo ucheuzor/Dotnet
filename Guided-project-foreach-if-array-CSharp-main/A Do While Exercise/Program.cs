cd8b5c78bda37c322f6e7e6e828c72fa880abeab
Urchy <ucheuzor24@gmail.com>
2024-10-22 10:20:11 +0100

    A simple game that has a winner when the health of one of the players is less than or equal to zero

---
+++ Guided-project-foreach-if-array-CSharp-main/A Do While Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/*
+This application is a simple game between a hero and a monster. Based on the strikes, we determine the health of the monster and the Hero. There is a winner if one of the player's health is less than or equal to zero
+*/
+
+int heroHealth = 10;
+int monsterHealth = 10;
+Random random = new Random();
+int heroStrike = random.Next(1, 11);
+int monsterStrike = 0;
+
+while (heroHealth > 0 && monsterHealth > 0)
+{
+    monsterHealth -= heroStrike;
+
+    //Check Monster health
+    if (monsterHealth > 0)
+    {
+        Console.WriteLine($"Monster was damaged and lost {heroStrike} health and now has {monsterHealth} health.");
+        monsterStrike = random.Next(1, 11);
+        heroHealth -= monsterStrike;
+
+    }
+    else
+    {
+        Console.WriteLine($"Monster was damaged and lost {heroStrike} health and now has {monsterHealth} health.\n Hero Wins!");
+        break;
+    }
+
+    //Check Hero health
+    if (heroHealth > 0)
+    {
+        Console.WriteLine($"Hero  was damaged and lost {monsterStrike} health and now has {heroHealth} health.");
+        heroStrike = random.Next(1, 11);
+    }
+    else
+    {
+        Console.WriteLine($"Hero was damaged and lost {monsterStrike} health and now has {heroHealth} health.\n Monster Wins!");
+    }
+
+}
