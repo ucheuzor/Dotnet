1f52e99cade50a270fa6546e6a9f868d7df3bc66
Urchy <ucheuzor24@gmail.com>
2024-10-23 19:51:57 +0100

    Added logic to get other information about the animal

--- Guided-project-branching-looping-CSharp-main/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ed443f4967fc590264b8d07cace45454707dd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86298855d87928e7652b7e03b5934b55f98a3a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
