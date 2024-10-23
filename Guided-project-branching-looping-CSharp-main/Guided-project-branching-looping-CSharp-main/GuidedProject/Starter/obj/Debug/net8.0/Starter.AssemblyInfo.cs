d3b73a38700ba6a3d6686e801d094bf5cf27007d
Urchy <ucheuzor24@gmail.com>
2024-10-23 20:09:37 +0100

    final changes completed

--- Guided-project-branching-looping-CSharp-main/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86298855d87928e7652b7e03b5934b55f98a3a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f52e99cade50a270fa6546e6a9f868d7df3bc66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
