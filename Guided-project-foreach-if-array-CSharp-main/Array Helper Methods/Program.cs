de83f142370e9fdbf82df7150bb4f6b8f4dd5491
Urchy <ucheuzor24@gmail.com>
2024-10-26 20:30:21 +0100

    Array expression to clear Array elements

---
+++ Guided-project-foreach-if-array-CSharp-main/Array Helper Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string[] pallets = ["B14", "A11", "B12", "A13"];
+
+Console.WriteLine("Sorted...");
+
+Array.Sort(pallets);
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"...{pallet}");
+}
+
+
+
+Array.Clear(pallets, 1, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"...{pallet}");
+}
