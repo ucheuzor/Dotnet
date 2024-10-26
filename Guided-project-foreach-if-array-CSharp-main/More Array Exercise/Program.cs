d683dac86cab55ef181f0ed5dd5db47fe0cf245e
Urchy <ucheuzor24@gmail.com>
2024-10-26 22:36:11 +0100

    Wrote a Program in C# to Reverse the each Character in a String

---
+++ Guided-project-foreach-if-array-CSharp-main/More Array Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿//This C# Program is written to reverse each character in a sentence.
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] splitedSentence = pangram.Split(' ');
+string[] reversedSentence = new string[splitedSentence.Length];
+
+for (int i = 0; i < splitedSentence.Length; i++)
+{
+    char[] charIndividualSentence = splitedSentence[i].ToCharArray();
+    Array.Reverse(charIndividualSentence);
+    string ReversedSentenceString = new string(charIndividualSentence);
+
+    reversedSentence[i] = ReversedSentenceString;
+}
+
+//Join reversed Sentence
+string joinReversedSentence = string.Join(' ', reversedSentence);
+
+Console.WriteLine($"Original Sentence: {pangram}");
+Console.WriteLine("");
+
+Console.WriteLine($"Reversed Sentence: {joinReversedSentence}");
