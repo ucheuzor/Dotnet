914007d554a1adc7d7d91796f11e2a156cbbd75c
Urchy <ucheuzor24@gmail.com>
2024-11-01 16:56:23 +0100

    code update

--- Guided-project-Work-with-variable-data-in-CSharp-main/Guided-project-Work-with-variable-data-in-CSharp-main/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Guided-project-Work-with-variable-data-in-CSharp-main/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdf394fc71e53df9e83fe22aff75ea31e28a5e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020c3ca6e74f04689667126212d86cfa2d93dfd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
