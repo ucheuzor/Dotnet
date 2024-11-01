443ccf497f99a9fc797279f2f07673193f9a52f5
Urchy <ucheuzor24@gmail.com>
2024-11-01 21:15:20 +0100

    Added helper method to check if array items are within range

---
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Methods/Check ValidIP.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace CheckValidIP
+{
+    public class CheckIP
+    {
+        public static bool CalculateNumbers(string ipv4Input)
+        {
+            string[] inputItems = ipv4Input.Split('.');
+
+            if (ValidateLength(inputItems) && ValidateZeroes(inputItems) && ValidateRange(inputItems))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool ValidateLength(string[] inputItem)
+        {
+
+            if (inputItem.Length == 4)
+                return true;
+            return false;
+
+        }
+        private static bool ValidateZeroes(string[] inputItem)
+        {
+            for (int i = 0; i < inputItem.Length; i++)
+            {
+                string getFirstCharacter = inputItem[i].Substring(0);
+
+                if (getFirstCharacter.Trim() == "0")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static bool ValidateRange(string[] inputItem)
+        {
+
+            for (int i = 0; i < inputItem.Length; i++)
+            {
+                int getFirstCharacter = 0;
+
+                if (int.TryParse(inputItem[i], out getFirstCharacter))
+                {
+                    if (getFirstCharacter < 0 || getFirstCharacter > 255)
+                        return false;
+                }
+                else
+
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
