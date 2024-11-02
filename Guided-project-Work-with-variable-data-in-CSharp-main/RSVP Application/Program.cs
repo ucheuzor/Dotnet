b2cf4bf2617ef354bd7de6416f2c3643bb0367d9
Urchy <ucheuzor24@gmail.com>
2024-11-02 09:47:54 +0100

    added optional parameters for methods

---
+++ Guided-project-Work-with-variable-data-in-CSharp-main/RSVP Application/Program.cs
@@ -0,0 +1,50 @@
+﻿
+string[] guestList = ["Rebecca", "Nadia", "Noor", "Jonte"];
+string[] rsvps = new string[10];
+int count = 0;
+
+//Call Methods
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+
+//Declare RSVP Method. When a parameter is assiged a default value, we are simply making the parameter optional.
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+//Declare ShowRSVP Method
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
