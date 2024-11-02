ea5ae8843334c1c5e650dba82600bc9ebb62ecc8
Urchy <ucheuzor24@gmail.com>
2024-11-02 10:25:50 +0100

    Given a MultiDimenstion array of emails, I wrote a method to retrieve the first 2 letter of the firstname, combine it with the lastname, then form an email using a the company domain

---
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Email Generator/Program.cs
@@ -0,0 +1,42 @@
+﻿
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+string internalDomain = "contoso.com";
+
+GenerateInternalEmailAddress(corporate, external, internalDomain, externalDomain);
+
+
+void GenerateInternalEmailAddress(string[,] corporateEmails, string[,] externalEmails, string internalDomain, string externalDomain = "microsoft")
+{
+
+    for (int i = 0; i < corporateEmails.GetLength(0); i++)
+    {
+        string extractedFirstName = corporateEmails[i, 0].ToLower().Substring(0, 2);
+        string lasttName = corporateEmails[i, 1].ToLower();
+        string emailAddress = $"{extractedFirstName}{lasttName}@{internalDomain}";
+
+        Console.WriteLine(emailAddress);
+    }
+
+    for (int i = 0; i < externalEmails.GetLength(0); i++)
+    {
+        // display external email addresses
+        string extractedFirstName = externalEmails[i, 0].ToLower().Substring(0, 2);
+        string lasttName = externalEmails[i, 1].ToLower();
+        string emailAddress = $"{extractedFirstName}{lasttName}@{externalDomain}";
+
+        Console.WriteLine(emailAddress);
+    }
+}
