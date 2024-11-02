fbf5ce2a861ba25f07cac3cb2a7f92fdacaf66d6
Urchy <ucheuzor24@gmail.com>
2024-11-02 09:12:06 +0100

    Working with immutable string

---
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Value and Reference Type Examples/Program.cs
@@ -0,0 +1,41 @@
+﻿
+//Checking for Value types
+int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+int Multiply(int a, int b, int c)
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+    return c;
+}
+
+//Checking value types
+int[] array = { 1, 2, 3, 4, 5 };
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
+
+//Immutable string test
