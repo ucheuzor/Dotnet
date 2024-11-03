91ad7ab95f26ce15ddb0cc2d6366c62a6958f0ea
Urchy <ucheuzor24@gmail.com>
2024-11-03 23:37:49 +0100

    Wrote a C# simple dice game to output if a user won or lost based on their toss against a random target value

---
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Mini Dice Game/Program.cs
@@ -0,0 +1,77 @@
+﻿
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        int target = random.Next(1, 6);
+        int roll = random.Next(1, 7);
+
+        WinOrLose(roll, target);
+
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
+void WinOrLose(int roll, int target)
+{
+    string response = "";
+    bool outcomeCheck = false;
+
+    Console.WriteLine($"Roll a number greater than {target} to win! your roll must between 1 - 6");
+
+    do
+    {
+        response = Console.ReadLine();
+        int outcome = 0;
+
+        if (int.TryParse(response, out outcome))
+        {
+            if (outcome < 1 || outcome > 6)
+            {
+                Console.WriteLine("entered value must between 1 - 6");
+            }
+            else
+            {
+                Console.WriteLine($"You rolled a {roll}");
+                if (roll > target)
+                {
+                    Console.WriteLine("You win!");
+                    outcomeCheck = true;
+                }
+                else
+                {
+                    Console.WriteLine("You lose!");
+                    outcomeCheck = true;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid response! enter a numeric value");
+        }
+
+    } while (outcomeCheck == false);
+
+}
+
+//
+
+bool ShouldPlay()
+{
+    string? userResponse = Console.ReadLine();
+
+    return userResponse?.Trim().ToLower() == "y" ? true : false;
+}
