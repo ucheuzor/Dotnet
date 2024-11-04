0802c698409d74bfe9f12276bfb3471617d8e26f
Urchy <ucheuzor24@gmail.com>
2024-11-04 10:45:56 +0100

    add method to randomize animal array

--- Guided-project-Work-with-variable-data-in-CSharp-main/Petting Zoo Application/Program.cs
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Petting Zoo Application/Program.cs
@@ -5,3 +5,24 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 ];
+
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+};
+
+// string[,] group = AssignGroup();
+
+Console.WriteLine("School A");
+
+// PrintGroup(group);
