a8cf6ea8be8885fe574994b06e5c6a8d0ddf0a3c
Urchy <ucheuzor24@gmail.com>
2024-11-04 11:24:41 +0100

    minor update

--- Guided-project-Work-with-variable-data-in-CSharp-main/Petting Zoo Application/Program.cs
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Petting Zoo Application/Program.cs
@@ -37,6 +37,15 @@ void RandomizeAnimals()
 string[,] AssignGroup(int groups = 6)
 {
     string[,] result = new string[groups, pettingZoo.Length / groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
 
     return result;
 }
