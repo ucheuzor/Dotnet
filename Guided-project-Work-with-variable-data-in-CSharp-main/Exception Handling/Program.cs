94f8cd28c2048a43d30fe2a5b7235dc638977151
Urchy <ucheuzor24@gmail.com>
2024-11-06 18:59:22 +0100

    code update

--- Guided-project-Work-with-variable-data-in-CSharp-main/Exception Handling/Program.cs
+++ Guided-project-Work-with-variable-data-in-CSharp-main/Exception Handling/Program.cs
@@ -1,4 +1,76 @@
 ﻿
+string[][] userEnteredValues =
+[
+            ["1", "2", "3"],
+            ["1", "two", "3"],
+            ["0", "1", "2"]
+];
+
+try
+{
+    Workflow1(userEnteredValues);
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+catch (DivideByZeroException ex)
+{
+
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
+}
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            Process1(userEntries);
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        catch (FormatException ex)
+        {
+
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values must be valid integers.");
+        }
+    }
+}
+
+
+/*
+
+
 // Prompt the user for the lower and upper bounds
 Console.Write("Enter the lower bound: ");
 int lowerBound = int.Parse(Console.ReadLine());
@@ -66,7 +138,6 @@ static decimal AverageOfEvenNumbers(int lowerBound, int upperBound)
 }
 
 
-/*
 try
 {
     OperatingProcedure1();
